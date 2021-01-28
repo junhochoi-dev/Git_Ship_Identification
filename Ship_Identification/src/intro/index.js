238f64c9a02e7c3ff5cb1f07ba6096c6ae09db9b
Dev-ChoiJunHo <cjh970422@gmail.com>
2021-01-28 13:33:16 +0000

    20210128 2233

--- Ship_Identification/src/intro/index.js
+++ Ship_Identification/src/intro/index.js
@@ -31,19 +31,30 @@ export default class Intro extends Component{
 			<ImageBackground source={require('/workspace/Ship_Identification/assets/img/login.jpeg')} style={{flex: 1, resizeMode: 'cover',}}>
 				<base.Content padder>
 					<base.Form style={{alignItems: 'center', justifyContent: 'center', margin: 10}}>
-						<base.Text style={{fontFamily: 'Nanum_Title', fontSize: 50, color: 'white', marginTop: 80}}>앱 접근 제한 경고</base.Text>
-						<base.Icon name='ios-warning' style={{color: 'red', fontSize: 300, marginTop: 20}}/>
-						<base.Text style={{fontFamily:'Nanum', color: 'white', marginTop: 20}}>
-							앱 선박확인체계는 허가받은 사용자만 사용할 수 있습니다. 부당한 방법으로 해당 앱에 접근하거나 앱 내의 정보를
-							삭제, 변경, 유출하는 사용자는 관련법령에 따라 처벌받게 됩니다. 모든 접속 시도는 로그로 기록되며 불법적인 접근 시도는 법적 제재를 받을 수 있습니다.
-						</base.Text>
-						<base.Text style={{fontFamily:'Nanum', color: 'white', marginTop: 20}}>
-							저작권 법 제 25조 제 2항에 의해 특수 목적으로 사용되는 저작물이 포함되어 있습니다. 앱 내 자료를 외부에 공개 또는 게시하는 것을 금지하며 이를
-							위반할 경우 법적 제재를 받을 수 있습니다. 선박확인체계에서 제공하는 모든 자료는 저작권법에 의하여 보호받으며 그 저작권은 선박확인체계에 있습니다.
-							따라서 홈페이지에서 제공하는 자료에 대해 무단복제 및 배포를 원칙적으로 금합니다.
-						</base.Text>
-						<base.Button block onPress={()=>this.props.navigation.navigate('Login')} style={{backgroundColor: 'white', marginTop: 30}}>
-							<base.Text style={{fontFamily: 'Nanum', color: 'black'}}>위 내용을 인지하였습니다</base.Text>
+						<base.Text style={{fontFamily: 'Nanum_Title', fontSize: 40, color: 'white', marginTop: 40}}>앱 접근 제한 경고</base.Text>
+						<base.Icon name='ios-warning' style={{color: 'red', fontSize: 300,}}/>
+						<base.Form style={{margin: 10,}}>
+							<base.Text style={{fontFamily:'Nanum', color: 'white',}}>
+								앱 선박확인체계는 허가받은 사용자만 사용할 수 있습니다. 부당한 방법으로 해당 앱에 접근하거나 앱 내의 정보를
+								삭제, 변경, 유출하는 사용자는 관련법령에 따라 처벌받게 됩니다. 모든 접속 시도는 로그서버에 기록되며 불법적인 접근 시도는 법적 제재를 받을 수 있습니다.
+							</base.Text>
+							<base.Text style={{fontFamily:'Nanum', color: 'white', marginTop: 20}}>
+								저작권 법 제 25조 제 2항에 의해 특수 목적으로 사용되는 저작물이 포함되어 있습니다. 앱 내 자료를 외부에 공개 또는 게시하는 것을 금지하며 이를
+								위반할 경우 법적 제재를 받을 수 있습니다.
+							</base.Text>
+							<base.Text style={{fontFamily:'Nanum', color: 'white', marginTop: 20}}>
+								선박확인체계에서 제공하는 모든 자료는 저작권법에 의하여 보호받으며 그 저작권은 선박확인체계에 있습니다.
+								따라서 홈페이지에서 제공하는 자료에 대해 무단복제 및 배포를 원칙적으로 금합니다.
+							</base.Text>
+							<base.Form style={{alignItems: 'center', justifyContent: 'center',}}>
+								<base.Text style={{fontFamily:'Nanum_Title', color: 'white', marginTop: 20, fontSize: 18, color: 'red'}}>
+									허가받지 않은 사용자는 즉시 이 앱을 종료 및 삭제하십시오
+								</base.Text>
+							</base.Form>
+						</base.Form>
+						<base.Button block onPress={()=>this.props.navigation.navigate('Login')}
+							style={{marginTop: 30, borderColor: 'white', backgroundColor: 'transparent', borderWidth: 1,}}>
+							<base.Text style={{fontFamily: 'Nanum', color: 'white'}}>위 내용을 인지하였고 허가받은 사용자입니다</base.Text>
 						</base.Button>
 					</base.Form>
 				</base.Content>
